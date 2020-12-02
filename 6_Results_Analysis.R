18776e1f7de3ef0abfc36f6980cd9baa2e67792a
KimberlyHinz <KimberlyHinz@users.noreply.github.com>
2020-12-01 23:04:40 -0700

    Create 6_Results_Analysis.R

---
+++ 6_Results_Analysis.R
@@ -0,0 +1,83 @@
+# This is the sixth R File for this project.
+
+# The following code analyzes the distance matrices.
+
+library("tidyverse")
+theme_set(theme_bw())
+
+setwd("C:/Users/Kim/OneDrive/2020_3Fall/Biology_396")
+
+#
+# Table 1. -----------------------------------------------------------------------------------------------------------------------------------------------
+# ```{r echo = FALSE, message = FALSE, fig.cap=TRUE}
+strain <- data.frame(Species = c("Mixta calida", "Mixta gaviniae", "Pantoea agglomerans", "Pantoea septica", "Erwinia amylovora",
+                                 "Erwinia tasmaniensis", "Tatumella ptyseos", "Tatumella saanichensis", 
+                                 "Enterobacter cloacae subsp cloacae", "Pseudomonas syringae pv syringae"),
+                     Strain = c("DSM_22759", "DSM_22758", "NBRC_102470", "LMG_5345", "CFBP_1232", "ET1/99", "NCTC_11468",
+                                "NML_06-3099", "ATCC_13047", "ICMP_3023"),
+                     `GenBank Accession No.` = c("GCA_002953215.1", "GCA_002953195.1", "GCA_001598475.1", "GCA_002095575.1", "GCA_000367625.2",
+                                                 "GCA_000026185.1", "GCA_900478715.1", "GCA_000439375.1", "GCA_000025565.1", "GCA_001401075.1"),
+                     Level = c("complete genome", "complete genome", "contigs", "contigs", "contigs", "complete genome", "complete genome", 
+                               "contigs", "complete genome", "scaffold"))
+# 
+# library("knitr")
+# kable(strain, caption = "Table 1. Genomes used in this study.")
+# ```
+
+# Table 2. -----------------------------------------------------------------------------------------------------------------------------------------------
+# ```{r echo = FALSE, message = FALSE, fig.cap=TRUE}
+phylo_models <- read.csv("9_Results_NT/Phylo_Models_NT.csv", stringsAsFactors = FALSE)
+PM <- data.frame(table(phylo_models$BM))
+
+colnames(PM) <- c("Model", "Number")
+
+PM <- mutate(PM,
+             Percent = round((Number / sum(Number)) * 100, digits = 2))
+# 
+# library("knitr")
+# kable(strain, caption = "Table 2. The number of genes that required each phylogenetic tree model according to model testing and the lowest BIC.")
+# ```
+
+# Table 3 ------------------------------------------------------------------------------------------------------------------------------------------------
+# ```{r echo = FALSE, message = FALSE, fig.cap=TRUE}
+calida_four <- read.csv("9_Results_NT/four_relatives_calida_NT.csv", stringsAsFactors = FALSE)
+
+FR_NT_cal <- data.frame(Species = c("P. septica", "P. agglomerans", "E. tasmaniensis", "E. amylovora",
+                                    "T. ptyseos", "T. saanichensis", "E. cloacae", "P. syringae"),
+                        Number_cal = c(sum(calida_four$Closest_Relative == "P_septica"), sum(calida_four$Closest_Relative == "P_agglomerans"), 
+                                       sum(calida_four$Closest_Relative == "E_tasmaniensis"), sum(calida_four$Closest_Relative == "E_amylovora"),
+                                       sum(calida_four$Closest_Relative == "T_ptyseos"), sum(calida_four$Closest_Relative == "T_saanichensis"),
+                                       sum(calida_four$Closest_Relative == "E_cloacae"), sum(calida_four$Closest_Relative == "P_syringae")))
+
+FR_NT_cal <- mutate(FR_NT_cal,
+                    Percent_cal = round((Number_cal / sum(Number_cal)) * 100, digits = 1))
+
+gaviniae_four <- read.csv("9_Results_NT/four_relatives_gaviniae_NT.csv", stringsAsFactors = FALSE)
+
+rel_num_perc <- FR_NT_cal %>%
+  mutate(Number_gav = c(sum(gaviniae_four$Closest_Relative == "P_septica"), sum(gaviniae_four$Closest_Relative == "P_agglomerans"), 
+                        sum(gaviniae_four$Closest_Relative == "E_tasmaniensis"), sum(gaviniae_four$Closest_Relative == "E_amylovora"),
+                        sum(gaviniae_four$Closest_Relative == "T_ptyseos"), sum(gaviniae_four$Closest_Relative == "T_saanichensis"),
+                        sum(gaviniae_four$Closest_Relative == "E_cloacae"), sum(gaviniae_four$Closest_Relative == "P_syringae")),
+         Percent_gav = round((Number_gav / sum(Number_gav)) * 100, digits = 1))
+
+# 
+# library("knitr")
+# kable(strain, caption = "Table 3. The closest relative to the Mixta sequences for both species in the nucleotide analysis.")
+# ```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 38191_ttuB in nucleotides WAS THE ONLY ONE WHOSE CLOSEST RELATIVES WERE NOT THE SAME FOR THE TWO MIXTA SPECIES! ONLY ONE!!
